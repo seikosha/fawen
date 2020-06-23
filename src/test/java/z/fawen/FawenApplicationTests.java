be7e48533a06edc0ab7e0cf65f45ab49bf0eb93f
seikosha <56125237@qq.com>
2020-06-23 22:21:25 +0800

    first commit

---
+++ src/test/java/z/fawen/FawenApplicationTests.java
@@ -0,0 +1,13 @@
+package z.fawen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FawenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
