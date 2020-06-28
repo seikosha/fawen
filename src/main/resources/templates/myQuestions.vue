df634e2b14b7de50007c43eac9fff82176489a63
seikosha <56125237@qq.com>
2020-06-28 21:05:48 +0800

    6th day working on file path

--- src/main/resources/templates/myQuestions.vue
+++ src/main/resources/templates/myQuestions.vue
@@ -1,90 +1,55 @@
 <template>
 
-    <div>宝贝在哪里</div>
-<!--    <div class="container">-->
-<!--        <nav class="navbar navbar-expand navbar-light bg-light">-->
-<!--            <a class="navbar-brand pl-5" href="#">法问</a>-->
-<!--            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">-->
-<!--                <span class="navbar-toggler-icon"></span>-->
-<!--            </button>-->
+        <div class="table-responsive">
+            <table class="table table-striped table-bordered table-hover">
+                <thead>
+                <tr class="active">
+                    <th>序号</th>
+                    <th>标题</th>
+                    <th>跟帖数</th>
+                    <th>最后更新时间</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>喝酒后开车被抓了怎么办？</td>
+                    <td>2</td>
+                    <td>#</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>我前妻结婚前借我钱，现在离婚了还没还怎么办？</td>
+                    <td>3</td>
+                    <td>#</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>我的交通违章严重不合理！我到哪里告交警？</td>
+                    <td>0</td>
+                    <td>#</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item"><a class="page-link text-dark" href="#">首页</a></li>
+                <li class="page-item"><a class="page-link text-dark" href="#">上一页</a></li>
+                <li class="page-item"><a class="page-link text-dark" href="#">1</a></li>
+                <li class="page-item"><a class="page-link text-dark" href="#">2</a></li>
+                <li class="page-item"><a class="page-link text-dark" href="#">3</a></li>
+                <li class="page-item"><a class="page-link text-dark" href="#">下一页</a></li>
+                <li class="page-item"><a class="page-link text-dark" href="#">尾页</a></li>
+            </ul>
+        </nav>
 
-<!--            <div class="collapse navbar-collapse" id="navbarSupportedContent">-->
-<!--                <ul class="navbar-nav mr-auto pl-5">-->
-<!--                    <li class="nav-item">-->
-<!--                        <a class="nav-link active" href="#">我的提问 <span class="sr-only">(current)</span></a>-->
-<!--                    </li>-->
-<!--                    <li class="nav-item pl-3">-->
-<!--                        <a class="nav-link" href="#">我的回答</a>-->
-<!--                    </li>-->
-<!--                    <li class="nav-item pl-3">-->
-<!--                        <a class="nav-link" href="#">我的个人页面</a>-->
-<!--                    </li>-->
-<!--                    <li class="nav-item pl-3">-->
-<!--                        <a class="nav-link" href="#">站内私信</a>-->
-<!--                    </li>-->
-<!--                </ul>-->
-
-<!--                <div id="welcome" class="text-info pr-5" >欢迎您！{{username}}</div>-->
-
-<!--                <form class="form-inline ">-->
-<!--                    <input class="form-control" type="search" placeholder="关键字" aria-label="Search">-->
-<!--                    <button class="btn btn-outline-success" type="submit">搜索</button>-->
-<!--                </form>-->
-<!--            </div>-->
-<!--        </nav>-->
-
-<!--        <div class="table-responsive">-->
-<!--            <table class="table table-striped table-bordered table-hover">-->
-<!--                <thead>-->
-<!--                <tr class="active">-->
-<!--                    <th>序号</th>-->
-<!--                    <th>标题</th>-->
-<!--                    <th>跟帖数</th>-->
-<!--                    <th>最后更新时间</th>-->
-<!--                </tr>-->
-<!--                </thead>-->
-<!--                <tbody>-->
-<!--                <tr>-->
-<!--                    <td>1</td>-->
-<!--                    <td>喝酒后开车被抓了怎么办？</td>-->
-<!--                    <td>2</td>-->
-<!--                    <td>#</td>-->
-<!--                </tr>-->
-<!--                <tr>-->
-<!--                    <td>2</td>-->
-<!--                    <td>我前妻结婚前借我钱，现在离婚了还没还怎么办？</td>-->
-<!--                    <td>3</td>-->
-<!--                    <td>#</td>-->
-<!--                </tr>-->
-<!--                <tr>-->
-<!--                    <td>3</td>-->
-<!--                    <td>我的交通违章严重不合理！我到哪里告交警？</td>-->
-<!--                    <td>0</td>-->
-<!--                    <td>#</td>-->
-<!--                </tr>-->
-<!--                </tbody>-->
-<!--            </table>-->
-<!--        </div>-->
-<!--        <nav aria-label="Page navigation example">-->
-<!--            <ul class="pagination">-->
-<!--                <li class="page-item"><a class="page-link text-dark" href="#">首页</a></li>-->
-<!--                <li class="page-item"><a class="page-link text-dark" href="#">上一页</a></li>-->
-<!--                <li class="page-item"><a class="page-link text-dark" href="#">1</a></li>-->
-<!--                <li class="page-item"><a class="page-link text-dark" href="#">2</a></li>-->
-<!--                <li class="page-item"><a class="page-link text-dark" href="#">3</a></li>-->
-<!--                <li class="page-item"><a class="page-link text-dark" href="#">下一页</a></li>-->
-<!--                <li class="page-item"><a class="page-link text-dark" href="#">尾页</a></li>-->
-<!--            </ul>-->
-<!--        </nav>-->
-<!--    </div>-->
 
 </template>
 
 <script>
 
-    export default {
 
-    }
 </script>
 
 <style scoped>
