55570556fb9c3ad07ee020ea35996c6fc48980bc
seikosha <56125237@qq.com>
2020-07-01 17:56:28 +0800

    8th day answer.vue revised

--- src/main/resources/frontend/src/components/answer.vue
+++ src/main/resources/frontend/src/components/answer.vue
@@ -1,9 +1,21 @@
 <template>
   <el-container>
     <el-main>
-      <span>您提过的问题：</span><br>
-      <el-col :span="2"><span>&nbsp;</span></el-col>
-      <el-col :span="20">
+      <span>最新问题：</span><br>
+      <el-col :span="5">
+        <el-aside id="leftside" width="200px">
+          <el-card class="box-card">
+            <div slot="header" class="clearfix">
+              <span>按照类型浏览案件</span>
+
+            </div>
+            <div v-for="o in 4" :key="o" class="text item">
+              {{'列表内容 ' + o }}
+            </div>
+          </el-card>
+        </el-aside>
+      </el-col>
+      <el-col :span="14">
         <div  style="margin-top: 15px">
         <el-table
           :data="tableData"
@@ -14,7 +26,6 @@
         <el-table-column
           prop="id"
           label="序号"
-          width="60"
           id="id"
           align="center">
         </el-table-column>
@@ -22,25 +33,26 @@
         <el-table-column
           prop="name"
           label="标题"
-          width="545"
           align="center">
         </el-table-column>
         <el-table-column
           prop="date"
           label="最后更新时间"
-          width="120"
           align="center">
         </el-table-column>
           <el-table-column
             prop="follow"
             label="跟帖数"
-            width="70"
             align="center">
           </el-table-column>
           <el-table-column
             prop="replyer"
             label="最新回复者"
-            width="120"
+            align="center">
+          </el-table-column>
+          <el-table-column
+            prop="type"
+            label="案件类型"
             align="center">
           </el-table-column>
         </el-table>
@@ -54,7 +66,29 @@
         </div>
 
       </el-col>
-      <el-col :span="2"><span>&nbsp;</span></el-col>
+
+      <el-col :span="5">
+        <el-aside id="rightside" width="200px">
+          <el-card class="box-card">
+            <div slot="header" class="clearfix">
+              <span>如何解答法律问题？</span>
+
+            </div>
+            <div v-for="o in 4" :key="o" class="text item">
+              {{'列表内容 ' + o }}
+            </div>
+          </el-card>
+          <el-card class="box-card">
+            <div slot="header" class="clearfix">
+              <span>历史精选答案</span>
+
+            </div>
+            <div v-for="o in 4" :key="o" class="text item">
+              {{'列表内容 ' + o }}
+            </div>
+          </el-card>
+        </el-aside>
+      </el-col>
 
     </el-main>
   </el-container>
@@ -80,24 +114,28 @@
             name: '我被人打了怎么办？',
             follow:1,
             replyer: 'Jacky',
+            type:'刑事'
           }, {
             id:2,
             date: '2016-05-04',
             name: '别人欠我钱没还怎么办',
             follow:2,
-            replyer: 'Becky'
+            replyer: 'Becky',
+            type:'民事'
           }, {
             id:3,
             date: '2016-05-01',
             name: '老婆要跟我离婚，怎么分财产？',
             follow:3,
             replyer: 'Frade',
+            type:'民事'
           }, {
             id:4,
             date: '2016-05-03',
             name: '我被车撞了怎么索赔？',
             follow:4,
-            replyer: 'Dinesh'
+            replyer: 'Dinesh',
+            type:'民事'
           }]
         }}
     }
@@ -106,5 +144,15 @@
 <style scoped>
 .block{
   float:left;
+}
+#rightside{
+  position:relative;
+  left:33px;
+}
+
+#leftside
+{
+  position:relative;
+  right:5px;
 }
 </style>
